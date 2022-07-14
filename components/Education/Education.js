d837b68340b3cddff208d8e6a2600693fed7d964
William Shi <33271663+wx02shi@users.noreply.github.com>
2022-07-14 14:43:54 -0400

    Update Education.js

--- components/Education/Education.js
+++ components/Education/Education.js
@@ -4,15 +4,21 @@ import { Box, Heading, HStack, Text, VStack } from "@chakra-ui/layout";
 
 const university = (
   <VStack align="start" spacing={4}>
-    <Heading align="left" size="lg">University of Waterloo</Heading>
+    <Heading align="left" size="lg">
+      University of Waterloo
+    </Heading>
     <Text>Bachelor&apos;s in Computer Science, Sept 2020 - Present</Text>
-    <Text>Participating in UW&apos;s coop program.</Text>
+    <Text>
+      Participating in UW&apos;s coop program. 91% cumulative average.
+    </Text>
   </VStack>
 );
 
 const ib = (
   <VStack align="start" spacing={4}>
-    <Heading align="left" size="lg">International Baccalaureate Programme</Heading>
+    <Heading align="left" size="lg">
+      International Baccalaureate Programme
+    </Heading>
     <Text>Victoria Park C.I., Sept 2018 - May 2020</Text>
     <Text align="left">
       HL courses: Mathematics, English A (Language and Literature), Computer
