fdcc54dd85aca01197434252e225d64cc17f4dfa
William Shi <33271663+wx02shi@users.noreply.github.com>
2023-05-04 00:49:32 -0400

    update project link

--- components/Projects/Projects.js
+++ components/Projects/Projects.js
@@ -5,7 +5,7 @@ import { Grid, GridItem, VStack } from "@chakra-ui/layout";
 
 const listProjects = [
     { img: "congestion.png", title: "Urban Congestion Modeling", text: "CNN determines traffic congestion from satellite images. Winner of MacHacks 2022", link: "https://devpost.com/software/using-context-aware-cnn-to-model-congestion-in-urban-areas" },
-    { img: "nodes.png", title: "Network Membership Protocol", text: "Network failure detection and message dissemination via gossip protocol multicasting", link: "https://devpost.com/software/using-context-aware-cnn-to-model-congestion-in-urban-areas" },
+    { img: "nodes.png", title: "Network Membership Protocol", text: "Network failure detection and message dissemination via gossip protocol multicasting", link: "https://github.com/wx02shi/Failure-Detection-Membership-Protocol" },
     { img: "chess.png", title: "Chess Project", text: "command-line chess game, built with C++. Incorporates OOP and design patterns", link: "https://github.com/wx02shi/chess-command-line" },
     { img: "personal.png", title: "Personal Website", text: "You're seeing it right now! Made with Next.js, Chakra UI, and deployed on Vercel.", link: "https://github.com/wx02shi/personal-website" },
 ]
